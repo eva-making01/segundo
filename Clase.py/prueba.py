069a8ca49bf3ce8df572b17482d6c8cebd20187f
eva-making01 <rcoronagonzalez999@gmail.com>
2025-10-14 12:05:13 -0600

    3

--- Clase.py/prueba.py
+++ Clase.py/prueba.py
@@ -1,5 +1,5 @@
 Alumnos = []
-print("Lista vacia:", Alumnos)
+print("Lista vacía:", Alumnos)
 
 def insertar_alumno(nombre, edad, carrera):
     alumno = {
@@ -7,4 +7,20 @@ def insertar_alumno(nombre, edad, carrera):
         "edad": edad,
         "carrera": carrera
     }
-    
+    Alumnos.append(alumno)
+nombre = input("Ingrese el nombre del alumno: ")
+edad = int(input("Ingrese la edad del alumno: "))
+carrera = input("Ingrese la carrera del alumno: ")
+insertar_alumno(nombre, edad, carrera)
+
+nombre2 = input("Ingrese el nombre del alumno: ")
+edad2 = int(input("Ingrese la edad del alumno: "))
+carrera2 = input("Ingrese la carrera del alumno: ")
+insertar_alumno(nombre2, edad2, carrera2)
+
+nombre3 = input("Ingrese el nombre del alumno: ")
+edad3 = int(input("Ingrese la edad del alumno: "))
+carrera3 = input("Ingrese la carrera del alumno: ")
+insertar_alumno(nombre3, edad3, carrera3)
+
+print("Alumno", Alumnos)
