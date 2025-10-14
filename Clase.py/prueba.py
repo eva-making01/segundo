4a94b496ae78347aa1c9f9f7793988c11d3d13ad
eva-making01 <rcoronagonzalez999@gmail.com>
2025-10-14 11:42:15 -0600

    2

--- Clase.py/prueba.py
+++ Clase.py/prueba.py
@@ -1,2 +1,10 @@
 Alumnos = []
 print("Lista vacia:", Alumnos)
+
+def insertar_alumno(nombre, edad, carrera):
+    alumno = {
+        "nombre": nombre,
+        "edad": edad,
+        "carrera": carrera
+    }
+    
